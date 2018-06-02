708eed7a59c01ca0bb1a3b5319e3cbc4a76fbe6d
Felipe Garcia <felipefegar@gmail.com>
2018-06-02 10:27:17 +0000

    Added version

--- examples/carousel/script.js
+++ examples/carousel/script.js
@@ -7,12 +7,12 @@ window.addEventListener('DOMContentLoaded', function () {
         buttons: true,
         dots: true,
         rewind: true,
-        autoplay: 1000,
+        autoplay: 0,
         responsive: {
             "0": { count: 1.5, touch: true, buttons: false },
             "480": { count: 2.5, touch: true, buttons: false },
-            "768": { count: 3, touch: false, dots: false },
-            "1440": { count: 4, touch: false, dots: false }
+            "768": { count: 3, touch: false, dots: false, autoplay: 1000 },
+            "1440": { count: 4, touch: false, dots: false, autoplay: 1000 }
         }
     };
 
